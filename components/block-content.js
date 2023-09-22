8355d22f39c2fb5fe185ece0717315cec234083e
Quinten <quinaiton@hotmail.com>
2023-09-22 11:27:25 +1000

    redeploy test

--- components/block-content.js
+++ components/block-content.js
@@ -1,8 +1,7 @@
-import React from 'react'
-import cx from 'classnames'
 import BlockContent from '@sanity/block-content-to-react'
-
+import React from 'react'
 import { blockSerializers } from '@components/block-serializers'
+import cx from 'classnames'
 
 const Content = ({ blocks, className }) => {
   if (!blocks) return null
@@ -10,7 +9,7 @@ const Content = ({ blocks, className }) => {
   return (
     <BlockContent
       renderContainerOnSingleChild
-      className={cx('rc', className)}
+      className={cx('rc ', className)}
       blocks={blocks}
       serializers={blockSerializers}
     />
