2d0d8a5f9c18ea7b1047337c638f9eedbc14da2b
quinten <quinaiton@hotmail.com>
2023-10-23 19:08:37 +1000

    updates proxy to use dynamic url

--- components/CheckoutForm.js
+++ components/CheckoutForm.js
@@ -97,7 +97,7 @@ export default function CheckoutForm({ products, paymentIntentId, setShipping, s
           }
         }
         const res = await fetch(
-          `http://localhost:3000/api/proxy`, {
+          `${process.env.NEXT_PUBLIC_BASE_URL}/api/proxy`, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
